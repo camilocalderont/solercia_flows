a5a6ac9ca82b4c4d296ff75b3d38d3e10f430781
“Luisito” <supelanosuarezluisenrique5@gmail.com>
2026-01-31 20:57:55 -0500

    se elimina bolt.diy del proyecto, se incluye el boki-api y la base de datos mongodb en el docker-compose.yml

--- bolt.diy/app/lib/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './useMessageParser';
-export * from './usePromptEnhancer';
-export * from './useShortcuts';
-export * from './StickToBottom';
-export * from './useEditChatDescription';
-export { default } from './useViewport';
-export { useFeatures } from './useFeatures';
-export { useNotifications } from './useNotifications';
-export { useConnectionStatus } from './useConnectionStatus';
